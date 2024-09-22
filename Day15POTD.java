3c7890cbb14feb8c3bce0c5bbcd5cc5121fcc943
Aakash Vetiraayan <aakashvallamkonda@gmail.com>
2024-09-22 23:02:08 +0530

    P
    Longest Prefix Suffix
    Difficulty: HardAccuracy: 27.91%Submissions: 129K+Points: 8
    Given a string of characters, find the length of the longest proper prefix which is also a proper suffix.
    
    NOTE: Prefix and suffix can be overlapping but they should not be equal to the entire string.
    
    Examples :
    
    Input: str = "abab"
    Output: 2
    Explanation: "ab" is the longest proper prefix and suffix. 
    Input: str = "aaaa"
    Output: 3
    Explanation: "aaa" is the longest proper prefix and suffix. 
    Expected Time Complexity: O(|str|)
    Expected Auxiliary Space: O(|str|)
    
    Constraints:
    1 ≤ |str| ≤ 106
    str contains lower case English alphabets
    Longest Prefix Suffix
    Difficulty: HardAccuracy: 27.91%Submissions: 129K+Points: 8
    Given a string of characters, find the length of the longest proper prefix which is also a proper suffix.
    
    NOTE: Prefix and suffix can be overlapping but they should not be equal to the entire string.
    
    Examples :
    
    Input: str = "abab"
    Output: 2
    Explanation: "ab" is the longest proper prefix and suffix. 
    Input: str = "aaaa"
    Output: 3
    Explanation: "aaa" is the longest proper prefix and suffix. 
    Expected Time Complexity: O(|str|)
    Expected Auxiliary Space: O(|str|)
    
    Constraints:
    1 ≤ |str| ≤ 106
    str contains lower case English alphabets

---
+++ Day15POTD.java
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+
+            String s = read.readLine();
+            Solution ob = new Solution();
+            System.out.println(ob.lps(s));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+class Solution {
+    int lps(String str) {
+        int n = str.length();
+        int[] lps = new int[n];
+        int length = 0;
+        int i = 1;
+        
+        while (i < n) {
+            if (str.charAt(i) == str.charAt(length)) {
+                length++;
+                lps[i] = length;
+                i++;
+            } else {
+                if (length != 0) {
+                    length = lps[length - 1];
+                } else {
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+            return lps[n - 1];
+    }
+}
