e1a2bc3a2d151f9d49f91bd00a0a411fad3375a7
Aakash Vetiraayan <aakashvallamkonda@gmail.com>
2024-10-03 18:35:42 +0530

    P

---
+++ Day23POTD.java
@@ -0,0 +1,70 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+import java.util.Collections;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        sc.nextLine(); // Consume the newline character
+
+        while (t-- > 0) {
+            String s = sc.nextLine();
+            String[] parts = s.split(" ");
+            List<Integer> nums = new ArrayList<>();
+            for (String part : parts) {
+                nums.add(Integer.parseInt(part));
+            }
+            Solution ob = new Solution();
+            List<Integer> ans = ob.findMajority(nums);
+            for (int num : ans) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+    public List<Integer> findMajority(List<Integer> nums) {
+        int n = nums.size();
+        int candidate1 = -1, candidate2 = -1;
+        int count1 = 0, count2 = 0;
+        
+        // Find the two potential candidates
+        for (int num : nums) {
+            if (num == candidate1) {
+                count1++;
+            } else if (num == candidate2) {
+                count2++;
+            } else if (count1 == 0) {
+                candidate1 = num;
+                count1 = 1;
+            } else if (count2 == 0) {
+                candidate2 = num;
+                count2 = 1;
+            } else {
+                count1--;
+                count2--;
+            }
+        }
+        
+        // Validation step: Check if the candidates appear more than n / 3 times
+        count1 = 0;
+        count2 = 0;
+        for (int num : nums) {
+            if (num == candidate1) count1++;
+            else if (num == candidate2) count2++;
+        }
+        
+        List<Integer> result = new ArrayList<>();
+        if (count1 > n / 3) result.add(candidate1);
+        if (count2 > n / 3) result.add(candidate2);
+        
+        return result.isEmpty() ? Collections.singletonList(-1) : result;
+    }
+}
